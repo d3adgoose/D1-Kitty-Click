486e6c4ee9fb87f1958126c96ea5c6f2200ebf68
d3adgoose <150769059+d3adgoose@users.noreply.github.com>
2024-10-06 20:52:36 -0700

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import "./style.css";
+
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const gameName = "My amazing game";
+document.title = gameName;
+
+const header = document.createElement("h1");
+header.innerHTML = gameName;
+app.append(header);
